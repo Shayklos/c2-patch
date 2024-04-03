ad8da45d5d96f4221b2e8d9c42005b83c306af22
zDEFz <a-development@posteo.de>
2024-04-03 03:16:48 +0200

    add icons

--- classloader/java/IconFetcher.java
+++ classloader/java/IconFetcher.java
@@ -1,5 +1,11 @@
 package org.dynarec;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class IconFetcher implements Talker<String> {
 
     @Override
@@ -15,8 +21,31 @@ public class IconFetcher implements Talker<String> {
 
     public static String fetchIcon(String md5Hash) throws Exception {
         // Construct the URL using the MD5 hash
-        String url = "https://www.gravatar.com/avatar/" + md5Hash + "?d=https://i.imgur.com/Gms07El.png";
+        String url = "https://www.gravatar.com/avatar/" + md5Hash;
+        // + "?d=https://i.imgur.com/Gms07El.png";
         System.out.println("fetchIcon() was called! ");
+        saveIcon(md5Hash, url);
         return url;
+    }
+
+    private static void saveIcon(String md5Hash, String url) throws Exception {
+        // Create directory if it doesn't exist
+        Files.createDirectories(Paths.get("icons"));
+
+        // Generate a filename based on the MD5 hash
+        String filename = "icons/" + md5Hash + ".png";
+        
+        // Check if the file already exists
+        if (Files.exists(Paths.get(filename))) {
+            System.out.println("Icon already exists: " + filename);
+            return;
+        }
+
+        // Open a connection to the URL
+        URI uri = URI.create(url);
+        try (InputStream inputStream = uri.toURL().openStream()) {
+            // Copy the input stream directly to the file
+            Files.copy(inputStream, Paths.get(filename), StandardCopyOption.REPLACE_EXISTING);
+        }
     }
 }
