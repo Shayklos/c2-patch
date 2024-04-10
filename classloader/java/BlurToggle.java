08825303ef95d1157efe7e0b204cd77de6d5b544
zDEFz <a-development@posteo.de>
2024-04-11 00:14:24 +0200

    proof of concept - cached class loading!

---
+++ classloader/java/BlurToggle.java
@@ -0,0 +1,41 @@
+package org.dynarec;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BlurToggle implements Talker<String> {
+
+    private static String blurStatus = null;
+
+    @Override
+    public String say(String message) {
+        if (blurStatus == null) {
+            try {
+		// Set your BlurValue here!
+                blurStatus = toggleBlurStatus("off");
+            } catch (NumberFormatException e) {
+                System.err.println("Error toggling Blur Status: " + e.getMessage());
+                blurStatus = "Error toggling Blur Status: " + e.getMessage();
+            }
+        }
+        return blurStatus;
+    }
+
+    public static String toggleBlurStatus(String status) {
+        float blurValue;
+        switch (status.toLowerCase()) {
+            case "on":
+                blurValue = 20.0f;
+                break;
+            case "off":
+                blurValue = 0.0f;
+                break;
+            default:
+                blurValue = 0.0f; // Default to off if an invalid status is provided
+                break;
+        }
+        System.out.println("blurValue returned: " + blurValue);
+        return String.valueOf(blurValue);
+    }
+}
--- dynarec-intellijidea/c2patch/src/main/java/org/dynarec/Dynarec.java
+++ dynarec-intellijidea/c2patch/src/main/java/org/dynarec/Dynarec.java
@@ -2,11 +2,14 @@ package org.dynarec;
 
 import com.javax0.sourcebuddy.Compiler;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 public class Dynarec {
     private static final Logger logger = Logger.getLogger(Dynarec.class.getName());
@@ -18,117 +21,73 @@ public class Dynarec {
             //   String url = takeStringReturnString("6d22a7dcc475c025d2725309c8bb5213", "/home/blu/git6/c2-patch/classloader/java/printstuff.java);
            // logger.info("Icon fetched successfully: " + url);
             // takeIntReturnString(1,"/home/blu/git6/c2-patch/classloader/java/AudioLoader.java");
-            // takeStringReturnFloat("off","/home/blu/git6/c2-patch/classloader/java/BlurToggle.java");
+            takeStringReturnFloat("off","/home/blu/c2-dynarec-unstable/c2-patch/classloader/java/BlurToggle.java");
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error executing", e);
         }
     }
+    private static final Map<String, Class<?>> compiledClasses = new ConcurrentHashMap<>();
 
-    public static <T> T takeAnyReturnAny(T message, String javafile) throws Exception {
-        try {
-            String sourceCode = readFileAsString(javafile);
-            Class<?> compiledClass = Compiler.compile(sourceCode);
 
-            // Ensure that Talker is parametrized with T
-            Talker<T> talker = (Talker<T>) getTalkerInstance(compiledClass);
-
-            return talker.say(message); // Return the value returned by the say method
-        } catch (Exception var5) {
-            throw new Exception("Error compiling", var5);
+    public static <T> T compileAndExecuteAndCache(T message, String javafile) throws Exception {
+        Class<?> compiledClass = compiledClasses.get(javafile);
+        if (compiledClass == null) {
+            compiledClass = Compiler.compile(readFileAsString(javafile));
+            compiledClasses.put(javafile, compiledClass);
         }
+
+        Talker<T> talker = (Talker<T>) getTalkerInstance(compiledClass);
+        return talker.say(message);
     }
 
     public static String takeIntReturnString(int input, String javafile) throws Exception {
-        // Tested working for e.g as a AudioLoader
-        try {
-            String sourceCode = readFileAsString(javafile);
-            Class<?> compiledClass = Compiler.compile(sourceCode);
+        return compileAndExecuteAndCache(Integer.toString(input), javafile);
+    }
 
-            // Ensure that Talker is parametrized with String
-            Talker<String> talker = getTalkerInstance(compiledClass);
-
-            // Convert int index to String before passing it to say method
-            return talker.say(Integer.toString(input)); // Return the value returned by the say method
-        } catch (Exception var5) {
-            throw new Exception("Error compiling", var5);
+    public static <T> T takeAnyReturnAny(T message, String javafile) throws Exception {
+        Class<?> compiledClass = compiledClasses.get(javafile);
+        if (compiledClass == null) {
+            compiledClass = Compiler.compile(readFileAsString(javafile));
+            compiledClasses.put(javafile, compiledClass);
         }
+
+        Talker<T> talker = (Talker<T>) getTalkerInstance(compiledClass);
+        return talker.say(message);
     }
 
     public static float takeStringReturnFloat(String message, String javafile) throws Exception {
-        // Tested working as e.g for setting blur toggle (float)
-        try {
-            String sourceCode = readFileAsString(javafile);
-            Class<?> compiledClass = Compiler.compile(sourceCode);
-
-            // Ensure that Talker is parametrized with String
-            Talker<String> talker = getTalkerInstance(compiledClass);
-
-            // Pass the message directly to the say method
-            String resultString = talker.say(message);
-            // Return the result string directly since it's already a Float
-            return Float.parseFloat(resultString);
-        } catch (Exception e) {
-            throw new Exception("Error compiling", e);
-        }
+        Talker<String> talker = getTalkerInstance(getCompiledClass(javafile));
+        return Float.parseFloat(talker.say(message));
     }
 
     public static String takeStringReturnString(String message, String javafile) throws Exception {
-       // Additional - untested
-        try {
-            String sourceCode = readFileAsString(javafile);
-            Class<?> compiledClass = Compiler.compile(sourceCode);
-
-            // Ensure that Talker is parametrized with String
-            Talker<String> talker = getTalkerInstance(compiledClass);
-
-            // Pass the message directly to the say method
-            // Return the result string directly since it's already a Float
-            return talker.say(message);
-        } catch (Exception e) {
-            throw new Exception("Error compiling", e);
-        }
+        Talker<String> talker = getTalkerInstance(getCompiledClass(javafile));
+        return talker.say(message);
     }
 
     public static void takeStringReturnVoid(String input, String javafile) {
-        // Additional - untested
         try {
-            // Read the Java source code from the specified file path
-            String sourceCode = readFileAsString(javafile);
-            // Compile the source code
-            Class<?> compiledClass = Compiler.compile(sourceCode);
-            // Create an instance of the compiled class using reflection
-            Talker talker = (Talker) compiledClass.getDeclaredConstructor(Object.class)
+            Talker talker = (Talker) getCompiledClass(javafile).getDeclaredConstructor(Object.class)
                     .newInstance(input);
-            // Call the say() method with appropriate arguments
             talker.say(input);
         } catch (Exception e) {
-            // Replace printStackTrace() with more robust logging
             System.err.println("Error compiling: " + e.getMessage());
         }
     }
 
-
-
     public static int takeStringReturnInt(String message, String javafile) throws Exception {
-        // Additional - untested
-        try {
-            String sourceCode = readFileAsString(javafile);
-            Class<?> compiledClass = Compiler.compile(sourceCode);
-
-            // Ensure that Talker is parametrized with String
-            Talker<String> talker = getTalkerInstance(compiledClass);
-
-            // Pass the message directly to the say method
-            // Return the result string directly since it's already a Float
-            return Integer.parseInt(talker.say(message));
-        } catch (Exception e) {
-            throw new Exception("Error compiling", e);
-        }
+        Talker<String> talker = getTalkerInstance(getCompiledClass(javafile));
+        return Integer.parseInt(talker.say(message));
     }
 
-
-
-
+    private static Class<?> getCompiledClass(String javafile) throws IOException, Compiler.CompileException, ClassNotFoundException {
+        Class<?> compiledClass = compiledClasses.get(javafile);
+        if (compiledClass == null) {
+            compiledClass = Compiler.compile(readFileAsString(javafile));
+            compiledClasses.put(javafile, compiledClass);
+        }
+        return compiledClass;
+    }
 
     // Method to read the contents of a file into a string
     private static String readFileAsString(String fileName) throws IOException {
