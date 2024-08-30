7696b693e2e43d6dafa3036f83e77000bfd827fe
bluetail <a-development+gitea@posteo.de>
2024-08-30 21:35:25 +0200

    attempt to automatically grab the proper path for windows c2settings and colorpicker

--- launchers/Windows-64-c2settings.bat
+++ launchers/Windows-64-c2settings.bat
@@ -1,1 +1,11 @@
-".\resources\graalvm-jdk-17.0.11+7.1\bin\java" -cp cultris2.jar c2settings
+# Set the base directory where the GraalVM folders are located
+$baseDir = ".\resources"
+
+# Find the directory that matches "graalvm" pattern
+$graalvmDir = Get-ChildItem -Path $baseDir -Filter "graalvm*" -Directory | Select-Object -First 1
+
+# Construct the path to the Java executable
+$javaPath = Join-Path $graalvmDir.FullName "bin\java"
+
+# Run the Java command with the classpath and the main class
+& $javaPath -cp "cultris2.jar" c2settings
