42c6cd37aab9c62e5bd4c63cf7ce7793445f7b04
zenith-devv <maciejektomaszek10@gmail.com>
2026-06-08 00:12:37 +0200

    New settings code implementation

--- scripts/Linux-64-c2settings.sh
+++ scripts/Linux-64-c2settings.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-SCRIPT_PATH="$(readlink -f "$0")" # Get the absolute path to this script
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
-cd "$SCRIPT_DIR/.." # move to project root
-export DIR="$(pwd)" # get absolute path of project root
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT="$SCRIPT_DIR/.."
+BASE_DIR="$ROOT/resources"
+JAVA_EXE="bin/java" 
 
-# Find the Java executable for JDK-17 Temurin
-export javaexec=$(find "$DIR/resources" -type f -name 'java' | grep 'jdk-17')
+LATEST_DIR=""
 
-# Convert absolute paths to relative for output
-javaexec_rel="${javaexec#$DIR/}"
+for dir in "$BASE_DIR"/*; do
+    if [ -d "$dir" ] && [ -x "$dir/$JAVA_EXE" ]; then
+        LATEST_DIR="$dir"
+    fi
+done
 
-# Always use Temurin Java executable
-java_to_use=$javaexec
-java_to_use_rel=$javaexec_rel
-echo "Using Temurin JDK Java executable at: $java_to_use_rel"
-
-cd "$DIR"
-
-# Check if the chosen Java executable is found and launch the application
-if [ -f "$java_to_use" ]; then
-    "$java_to_use" -Djava.library.path="$DIR/resources/libs/" -cp "$DIR/cultris2.jar" c2settings
+if [ -n "$LATEST_DIR" ]; then
+    cd "$ROOT" || exit 1
+    "$LATEST_DIR/$JAVA_EXE" -cp "settings/bin" c2settings
 else
-    echo "Java executable not found. Please ensure you've installed Temurin JDK correctly in the resources directory."
+    echo "No directory with java executable found in the resources folder."
+    exit 1
 fi
--- scripts/Windows-64-c2settings.bat
+++ scripts/Windows-64-c2settings.bat
@@ -1,24 +1,21 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Set the base directory to search for Java executables
-set "scriptDir=%~dp0"
-set "baseDir=%scriptDir%resources"
-set "javaExe=bin\java.exe"
+set "ROOT=%~dp0..\"
+set "baseDir=%ROOT%resources"
+set "javaExe=bin\javaw.exe"
 set "latestDir="
 
-rem Loop through all subdirectories in the base directory
 for /D %%I in ("%baseDir%\*") do (
-    rem Check if the directory contains the Java executable
     if exist "%%I\%javaExe%" (
         set "latestDir=%%I"
     )
 )
 
-rem If a valid Java directory is found, execute the ColorPicker
 if defined latestDir (
-    echo Found Java in: !latestDir!
-    start "c2settings" /high "!latestDir!\%javaExe%" -cp "cultris2.jar" c2settings
+    cd /d "%ROOT%"
+    "!latestDir!\%javaExe%" -cp "%~dp0bin" c2settings
+    pause
 ) else (
     echo No directory with java.exe found in the resources folder.
     pause
